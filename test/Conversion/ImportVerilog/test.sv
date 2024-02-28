5d7d1d7e0fb543f00a537a5c8a46c71e5f401a4c
BuyunXu <buyun.xu@terapines.com>
2024-02-28 14:28:20 +0800

    [Moore] Add a new replicate operation and introduce the support for elementSelect & rangeSelect
    1. Fix and add more details for extract op in MooreOps.td.
    2. Refactor timingControl in the ImportVerilog pass.
    3. Add support for the RangeSelect & ElementSelect features that are used on translatiing packed / unpacked type with its operations
    4. Add corresponding test cases for newly implemented features.
    
    Author: hailongSun2000 - hailong.sun@terapines.com

--- test/Conversion/ImportVerilog/test.sv
+++ test/Conversion/ImportVerilog/test.sv
@@ -6,3 +6,21 @@
 // CHECK-NEXT: %b = moore.port Out : !moore.packed<range<logic, 3:0>>
 module Port(input [3:0] a, output [3:0] b);
 endmodule
+
+// CHECK-LABEL: moore.module @DFF {
+module DFF(
+    input clk,
+    input d,
+    input rst,   // Asynchronous reset
+    output reg q);
+    
+    // CHECK: moore.procedure always {
+    // CHECK:   moore.event posedge %clk : !moore.logic
+    // CHECK:   moore.event posedge %rst : !moore.logic
+    always @(posedge clk or posedge rst) begin
+        if(rst)
+            q <= 1'b0;
+        else
+            q <= d;
+    end
+endmodule
